ca31e479353d2430d5518aeb3490f11045ea082c
7Aces <sanyam.satia@ymail.com>
2015-11-07 17:43:23 -0500

    fixed er

--- testhttp/ViewController.swift
+++ testhttp/ViewController.swift
@@ -13,18 +13,14 @@ class ViewController: UIViewController {
     func postToServerFunction() {
         
         let url = NSURL(string: "http://projecthashtag.website/comm.php?query=getevents");
-        var dta = NSArray();
         
         // modify the request as necessary, if necessary
         
         let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
             print(NSString(data: data!, encoding: NSUTF8StringEncoding))
-            dta = NSJSONSerialization.JSONObjectWithData(data!, options: nil, error: nil) as NSArray;
         }
         
         task.resume()
-        
-        
         
     }
 
